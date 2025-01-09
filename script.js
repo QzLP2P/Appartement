dcb7a425eb4dfe72aa6a9149d9b5986804273e78
Thomas GICQUEL <m.gicquel.thomas@gmail.com>
2025-01-09 10:36:48 +0100

    test

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const totalImages = 24; // Nombre total d'images
 
 // Générer les images de photo0.jpg à photo23.jpg
 for (let i = 0; i < totalImages; i++) {
+    const container = document.createElement('div');
+    container.classList.add('image-container');
+
     const img = document.createElement('img');
     img.src = `images/photo${i}.jpg`; // Chemin vers les images
     img.alt = `Photo ${i}`;
     img.classList.add('gallery-image');
-    if (i === 0) img.classList.add('active'); // Afficher la première image
-    gallery.appendChild(img);
-}
 
+    container.appendChild(img);
+    gallery.appendChild(container);
+}
 // Gestion de la navigation
 const images = document.querySelectorAll('.gallery-image');
 const prevButton = document.getElementById('prev');
